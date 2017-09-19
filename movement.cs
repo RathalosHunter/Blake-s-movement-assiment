7e701affcfdb34cc2ee7a0f3cf476b0b77f8689c
TopNowa <bsrblake@optusnet.com.au>
2017-09-19 17:21:46 +1000

    Here is my movement script.

---
+++ movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+	public float movespeed;
+	public float jumpforce;
+	private Rigidbody rigid;
+	// Use this for initialization
+	void Start () 
+	{
+		rigid = GetComponent<Rigidbody> ();
+		/*recomended values for script. 
+		movespeed = 10f;
+		jumpforce = 500f;*/
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (movespeed * Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, movespeed * Input.GetAxis ("Vertical") * Time.deltaTime);
+	}
+
+	void FixedUpdate()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			rigid.AddForce (Vector3.up * jumpforce);
+		}
+	}
+}
